0e3be718d9276bdda526b712496708713f0c0075
w1lldone <wildan.arrahman@gmail.com>
2019-06-21 08:14:54 +0700

    Add clients class

---
+++ app/Clients/BaseClient.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Clients;
+
+abstract class BaseClient 
+{
+    
+}
---
+++ app/Clients/MicrosoftAcademicClient.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Clients;
+
+use GuzzleHttp\Client;
+use App\Formatters\FormatterInterface;
+
+class MicrosoftAcademicClient  
+{
+    protected $client;
+
+    public function __construct($apiKey = null) {
+        $this->client = new Client([
+            'base_uri' => 'https://api.labs.cognitive.microsoft.com/academic/v1.0/',
+            'headers' => [
+                'Ocp-Apim-Subscription-Key' => $apiKey
+            ]
+        ]);
+    }
+
+    public function evaluate(array $params = [], $year = 2019, FormatterInterface $formatter = null)
+    {
+        $params = array_merge( 
+            [
+                'expr' => "AND(Composite(AA.AfN=='gadjah mada university'),Y=$year,L='en') ",
+                'attributes' => 'Id,C.CId,C.CN,L,Y,Ti,CC,J.JN,J.JId,AA.AuN,AA.AfN,E.DOI',
+                'offset' => 0,
+                'count' => 100,
+            ], $params
+        );
+
+        $reponse = $this->client->post('evaluate', [
+            'form_params' => $params
+        ]);
+
+        $body = json_decode($reponse->getBody(), true);
+
+        if ($formatter) {
+            return $formatter->format($body);
+        }
+
+        return $body;
+    }
+}
