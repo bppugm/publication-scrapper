3a6b08aa24d270fb4a914d8294f99ef791fabe6e
w1lldone <wildan.arrahman@gmail.com>
2019-06-21 08:18:08 +0700

    Add Ms Academic Controller

---
+++ app/Http/Controllers/MicrosoftAcademicController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Jobs\MicrosoftAcademicScrapper;
+
+class MicrosoftAcademicController extends Controller
+{
+    public function index(Request $request)
+    {
+        return view('microsoft-academic.index');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'year' => 'required',
+            'email' => 'required|email'
+        ]);
+
+        MicrosoftAcademicScrapper::dispatchNow($request->email, $request->year);
+
+        return redirect()->route('microsoft-academic.index')
+        ->with('success', 'Your request has been processed. Please check your email in a couple minutes.');
+    }
+}
