ac21606d97293d448bd7874b2351287fdc12ece8
w1lldone <wildan.arrahman@gmail.com>
2021-04-06 10:43:50 +0700

    Author document export command

---
+++ app/Console/Commands/AuthorDocumentExportCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Exports\AuthorDocumentExport;
+use Illuminate\Console\Command;
+
+class AuthorDocumentExportCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'author_document:export';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Export author document pivot sheets';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $export = new AuthorDocumentExport;
+        $export->store('author_document.xlsx', 'local');
+    }
+}
