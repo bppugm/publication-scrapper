5f4fc0a912fe8a1b1b238b3dd5b6e49ed1abf9b1
w1lldone <wildan.arrahman@gmail.com>
2022-07-01 10:18:42 +0700

    Fix Author Command

--- app/SimasterAuthor.php
+++ app/SimasterAuthor.php
@@ -9,4 +9,23 @@ class SimasterAuthor extends Model
     protected $collection = 'simaster_authors';
     protected $connection = 'mongodb';
     protected $guarded = ['id'];
+
+    public static function searchText($value)
+    {
+        $query = self::whereRaw([
+            '$text' => [
+                '$search' => $value
+            ]
+        ]);
+        $query->getQuery()->projections = ['score' => ['$meta' => 'textScore']];
+        $search = $query->orderBy('score', ['$meta' => 'textScore'])->get();
+        if ($search->count()) {
+            $search = $search->first();
+            if ($search->score >= 1) {
+               return $search;
+            }
+        }
+
+        return null;
+    }
 }
